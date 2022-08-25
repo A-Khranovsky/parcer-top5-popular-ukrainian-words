77644f75a5045b96bf19b14144532a7e090fe337
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-08-25 19:51:32 +0300

    Realized division

---
+++ Test.php
@@ -0,0 +1,53 @@
+<?php
+
+$source = <<<SOURCE
+основний елемент ферма гау є пояс верхній і ніжній майже завжди він паралельний
+проте для великий прогон є приклад полігональний у цей випадок конструкція
+ферма значно ускладнюватися і вона втратити одна зі свій перевага простота
+виготовлення і монтаж крім той полігональний ферма неможливо
+SOURCE;
+
+$words = [];
+$pattern = '/([а-яіїє]+)/ui'; //все слова
+preg_match_all($pattern, $source, $words);
+//echo !empty($words) ? count($words[0]) : null;
+
+//var_dump($words[0]);
+
+//echo preg_match_all('/\bі\b/ui',$source);
+
+$result = [];
+foreach ($words[0] as $word) {
+    $result[$word] = preg_match_all('/\b' . $word . '\b/ui', $source);
+}
+//arsort($result);
+
+function f($source, &$number)
+{
+    foreach ($source as $key => $item) {
+        if ($item == $number) {
+            yield $key => $item;
+        }
+    }
+}
+
+$buff = [];
+for ($i = 1; $i <= 3; $i++) {
+    foreach (f($result, $i) as $key => $item) {
+        $buff[$i][$key] = $item;
+    }
+}
+
+var_dump($buff);
+//$pattern = '/[^\s]/u'; //вск букві без пробелов
+//preg_match_all($pattern, $source, $matches);
+//if (!empty($matches)) {
+//    var_dump($matches[0]);
+//}
+
+//$pattern = '/(.|\s)/u'; //все буквы и пробелы
+//preg_match_all($pattern, $source, $matches);
+//if (!empty($matches)) {
+//    var_dump($matches[0]);
+//}
+
